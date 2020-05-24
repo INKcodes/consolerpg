721972536e7841484031897e53ba8f8ba7b99c35
Ingolf Kamen <ingolf.kamen@googlemail.com>
2020-05-24 19:44:50 +0200

    player sprite added

--- src/main/java/com/inkcodes/rpg/game/GameEngine.java
+++ src/main/java/com/inkcodes/rpg/game/GameEngine.java
@@ -1,17 +1,37 @@
 package com.inkcodes.rpg.game;
 
+import com.googlecode.lanterna.Symbols;
 import com.googlecode.lanterna.input.KeyStroke;
 import com.inkcodes.rpg.Engine;
 import com.inkcodes.rpg.graphics.GraphicsFactory;
+import com.inkcodes.rpg.graphics.Sprite;
 
 public class GameEngine implements Engine {
 
   private final GraphicsFactory graphicsFactory;
+  private final Sprite playerSprite;
 
   public GameEngine(final GraphicsFactory graphicsFactory) {
     this.graphicsFactory = graphicsFactory;
+    this.playerSprite = graphicsFactory.createSprite(Symbols.CLUB);
   }
 
   @Override
-  public void tick(final KeyStroke keyStroke) {}
+  public void tick(final KeyStroke keyStroke) {
+    // TODO: still very empty here :(
+    switch (keyStroke.getKeyType()) {
+      case ArrowDown:
+        playerSprite.setPosY(playerSprite.getPosY() + 1);
+        break;
+      case ArrowUp:
+        playerSprite.setPosY(playerSprite.getPosY() - 1);
+        break;
+      case ArrowRight:
+        playerSprite.setPosX(playerSprite.getPosX() + 1);
+        break;
+      case ArrowLeft:
+        playerSprite.setPosX(playerSprite.getPosX() - 1);
+        break;
+    }
+  }
 }
